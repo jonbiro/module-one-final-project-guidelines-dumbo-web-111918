3638af87cb099604f8dd313e50244a5c2610b962
Thompson Plyler <nealok.g@gmail.com>
2018-12-06 11:59:08 -0500

    Back end actually works now.

--- lib/order_up.rb
+++ lib/order_up.rb
@@ -3,7 +3,9 @@ def user_ingredients(tty_user)
 end
 
 def all_ingredients_all_recipes
-  Recipe.all.collect(&:ingredients)
+  Recipe.all.collect do |recipeObj|
+    recipeObj.ingredients.to_a
+  end
 end
 
 def find_overlap(tty_user)
@@ -16,21 +18,42 @@ def find_overlap(tty_user)
 end
 
 def compare_overlap(tty_user)
+
   recipe_matches = []
+
   find_overlap(tty_user).each do |ovrlapArr|
     all_ingredients_all_recipes.each do |ingList|
-      recipe_matches << ingList if ovrlapArr == ingList
+      # binding.pry
+      if ovrlapArr == ingList
+        recipe_matches << ingList
+      # end
+      # if a123 == a1234
+      #   recipe_matches << ovrlapArr
+      # else
+      #   return "no match"
+      end
     end
-  end
-  recipe_matches
   end
 
+  recipe_matches
+
+end
+
 def select_matching_recipes(tty_user)
+
   recipe_returns = []
+
   compare_overlap(tty_user).each do |ovrlapArr|
+    # puts "The overlapp array is #{ovrlapArr}"
     Recipe.all.each do |rcpObj|
-      recipe_returns << rcpObj.name if rcpObj.ingredients == ovrlapArr
+      # puts "  The recipe getting checked is #{rcpObj}."
+      # binding.pry
+      if rcpObj.ingredients == ovrlapArr
+        recipe_returns << rcpObj.name
+      end
     end
   end
-  recipe_returns
+
+  recipe_returns.uniq
+
 end
