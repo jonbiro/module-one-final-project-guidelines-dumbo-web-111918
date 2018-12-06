43939c37230e52262c2721e53fa0ad27b51cad86
jonbiro <jonnybrx@gmail.com>
2018-12-06 16:44:12 -0500

    works!

--- bin/run.rb
+++ bin/run.rb
@@ -1,17 +1,25 @@
 require_relative '../config/environment.rb'
-
 welcome
-username
-new_user
-recipe_return
 
-# username = username.downcase
-#
-#
-#
-# if user_exists(username)
-#   menu_screen(username)
-# else
-#   new_user(username)
-#   menu_screen(username)
-# end
+puts "
+"
+
+user = TTY::Prompt.new.select("Hello! Are you a new user or current user?", %w(New_User Current_User Exit))
+  if user == "New_User"
+    username
+  elsif user == "Current_User"
+    returning_user
+  elsif user == "Exit"
+    puts "See You Next Time!
+
+    "
+    exit
+  end
+
+
+
+
+# welcome
+# username
+# new_user
+# recipe_return
--- lib/cli.rb
+++ lib/cli.rb
@@ -12,38 +12,78 @@ end
 def username
   puts "What's your name?"
   username = gets.chomp
-  puts "Hi #{username}"
-  $nuser = User.create(name: username)
-  # username = User.create(name: username)
+  if username == "Stephen"
+    puts "Please don't break our app. Go away Stephen. Just kidding. But seriously...
+
+    "
+  elsif username == "Steve"
+    puts "Please don't break our app. Go away Stephen. Just kidding. But seriously...
+
+    "
+  elsif username == "steve"
+    puts "Please don't break our app. Go away Stephen. Just kidding. But seriously...
+
+    "
+  elsif username == "stephen"
+    puts "Please don't break our app. Go away Stephen. Just kidding. But seriously...
+
+    "
+  elsif username == "Steven"
+    puts "Please don't break our app. Go away Stephen. Just kidding. But seriously...
+
+    "
+  else
+    puts "Hi #{username}"
+    $nuser = User.create(name: username)
+    new_user
+  end
 end
 
+def username1
+  puts "What's your name again?"
+  username = gets.chomp
+  $nuser = User.find_by(name: username)
+end
 # def user_exists(username)
 #   exist = User.find_by(name: username)
 #   !!exist
 # end
 
 def new_user
+  system 'clear'
   prompt = TTY::Prompt.new
   # User.create(name: name)
   puts "use SPACEBAR to select the ingredients you have and Press ENTER when done."
   ingred = Ingredient.all.map { |ingredient| ingredient.name}
-  options = prompt.multi_select("Pick the ingredients you would like to use:", ingred)
-  # if options.empty?
-  #   puts "Please select at least one ingredient."
-  # else
+  options = prompt.multi_select("Pick the ingredients you have:", ingred, per_page: 20)
+  if options.empty?
+    puts "Please select at least one ingredient.
 
-
+    "
+  else
+    # UserIngredient.destroy_all
+    # RecipeIngredient.destroy_all
     options.each do |ing|
- UserIngredient.create(user: $nuser, ingredient: Ingredient.find_by(name: ing))
+
+      if(UserIngredient.where(user: $nuser, ingredient: Ingredient.find_by(name: ing)).size > 0)
+        $nuser.ingredients.destroy_all
+        UserIngredient.create(user: $nuser, ingredient: Ingredient.find_by(name: ing))
+      else
+        UserIngredient.create(user: $nuser, ingredient: Ingredient.find_by(name: ing))
+      end
+    end
   end
+  recipe_return
 end
 
 def recipe_return
   if select_matching_recipes.length < 1
-    puts "You don't have enough ingredients to make anything. Maybe it's time to go shopping."
+    puts "You don't have enough ingredients to make anything. Maybe it's time to go shopping.
+
+    "
   else
-    puts "Yummy! You can make"
-    puts select_matching_recipes.collect {|x|x}
+    puts "Yummy! You can make:"
+    select_matching_recipes.each_with_index {|x,index|puts "#{index+1}. #{x}"}
   end
 end
 
@@ -56,34 +96,40 @@ def name_call
   $nuser
 end
 
-def menu
-  prompt = TTY::Prompt.new
-  prompt.select("Let's see your options") do |menu|
-    #menu.enum '.'
-    menu.choice 'What can I make to eat?', 1
-    menu.choice 'Create or edit ingredient inventory', 2
-    menu.choice 'See my ingredients', 3
+def test_list
+
+  puts "Here is your list of ingredients:"
+  $nuser.ingredients.collect do |x|
+    puts "#{x.name}"
+  end
+end
+
+def returning_user
+  username1
+  $option = TTY::Prompt.new.select( "What would you like to do?") do |menu|
+    menu.choice 'List Your Inventory', 1
+    menu.choice 'Edit Your Ingredients', 2
+    menu.choice 'View Your Recipes', 3
     menu.choice 'Exit', 4
-  end
+    end
+
+
+
+    case $option
+    when 1
+
+      test_list
+
+
+    when 2
+      new_user
+
+    when 3
+      # binding.pry
+      recipe_return
+    when 4
+      puts "Goodbye!!!!!!!!!!!!"
+
+      exit
+    end
 end
-
-def delegate(choice)
-  case choice
-  when 1
-    puts "Here are your options to make with those ingredients"
-  when 2
-    puts "choose your ingredients"
-  when 3
-    puts "Here is what you have"
-  when 4
-    puts "Goodbye"
-  end
-
-end
-
-
-# prompt = TTY::Prompt.new
-# prompt.ask('What is your name?', default: ENV['USER'])
-#
-# choices = %w(cheese meat bread butter)
-# prompt.multi_select("Select with spacebar the food you have", choices)
