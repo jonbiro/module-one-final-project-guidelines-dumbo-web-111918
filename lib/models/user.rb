451a181a941a2bdb1087c4c6c922f50c5f78f93f
jonbiro <jonnybrx@gmail.com>
2018-12-06 09:55:48 -0500

    ,

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -4,8 +4,4 @@ class User < ActiveRecord::Base
   has_many :ingredients, through: :user_ingredients
   has_many :recipe_ingredients, through: :ingredients
   has_many :recipes, through: :recipe_ingredients
-<<<<<<< HEAD
-=======
-
->>>>>>> d9f96315c842c19ce97a9fe2463579581654eba9
 end
